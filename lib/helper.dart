7a2d4c5f216397d9b4e55f3bf484c51c26ae903b
Omar Qataberi <qataberio@gmail.com>
2023-09-16 01:43:54 +0300

    added abstract helper class

---
+++ lib/helper.dart
@@ -0,0 +1,30 @@
+import 'package:shorty/models/models.dart';
+
+abstract class Helper {
+  //collections
+  Future<void> addCollection(String name);
+  Future<void> updateCollection(int id, String name);
+  Future<void> deleteCollection(int id);
+  Future<Collecion> getCollection(int id);
+  Future<List<Collecion>> getCollections();
+
+  //shortcuts
+  Future<void> addShortcut(
+    String name,
+    String url,
+    int collection,
+    String? color,
+    String? imageUrl,
+  );
+  Future<void> updateShortcut(
+    int id,
+    String name,
+    String url,
+    int collection,
+    String? color,
+    String? imageUrl,
+  );
+  Future<void> deleteShortcut(int id);
+  Future<Shortcut> getShortcut(int id);
+  Future<List<Shortcut>> getShortcuts();
+}
